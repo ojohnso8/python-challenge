f7ab696f09609f5ef3e0f1fd43439067e01171cc
ojohnso8 <46421819+ojohnso8@users.noreply.github.com>
2019-03-26 19:43:54 -0400

    Third Commit

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -5,7 +5,7 @@ import csv
 #csv path
 budget_csv = os.path.join(".", "budget_data.csv")
 
-#empty lists for months and revenue
+#Must create empty lists for months and revenue
 months = []
 revenue = []
 
@@ -17,24 +17,18 @@ with open(budget_csv, newline = "") as csvfile:
 #append to put data from row[0] and row[1] into the lists created above
     for row in csvreader:
         months.append(row[0])
-        revenue.append(row[1])
+        revenue.append(int(row[1]))
 
-#total months
-    # total_months = len(months)
-    # print("Total Months: ", total_months)
+# #total months
+#     total_months = len(months)
+#     print("Total Months: ", total_months)
 
-# The net total amount of "Profit/Losses" over the entire period
-    revenue = 0
-    total_amount = sum(revenue)
-    print(total_amount)
+# # The net total amount of "Profit/Losses" over the entire period
+#     total_amount = sum(revenue)
+#     print("Total: $",total_amount)
 
-    # for row in csvreader:
-    #    print(float(sum(row[1]))
 # The average of the changes in "Profit/Losses" over the entire period
+
 # The greatest increase in profits (date and amount) over the entire period
+
 # The greatest decrease in losses (date and amount) over the entire period
-
-
-
-    # for row in csvreader:
-    #     print(row)
