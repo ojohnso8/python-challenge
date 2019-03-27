4e2bc29cc0bbcf4211eeec592fd88d8928a8130d
ojohnso8 <46421819+ojohnso8@users.noreply.github.com>
2019-03-27 17:55:58 -0400

    Seventh Commit

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -2,10 +2,10 @@
 import os
 import csv
 
-#csv path
+#create csv path
 budget_csv = os.path.join(".", "budget_data.csv")
 
-#Must create empty lists for months and revenue
+#Create empty lists for months and revenue
 months = []
 revenue = []
 
@@ -14,29 +14,22 @@ with open(budget_csv, newline = "") as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
     csv_header = next(csvreader)
 
-#append to put data from row[0] and row[1] into the lists created above
+
+#append month and revenue data from csv into empty lists created in lines 9 and 10
     for row in csvreader:
         months.append(row[0])
         revenue.append(int(row[1]))
-        # revenue_change.append(int(row[1]))
 
-#total months
+#calculate total months
     total_months = len(months)
     print("Total Months: ", total_months)
 
-# The net total amount of "Profit/Losses" over the entire period
+#calculate the net total amount of "Profit/Losses" over the entire period
     total_amount = sum(revenue)
     print("Total: $",total_amount)
 
-# The average of the changes in "Profit/Losses" over the entire period
-# you have to find calculate the % change for each row...
-# then add the averages
-# then divide average total by the number of months
-# [(new value - old value) / old value] * 100 
-
-    # average_change = round(total_amount/total_months, 2)
-    # print("Average Change: $", average_change)
-    
+ #calculate the average of the changes in "Profit/Losses" over the entire period 
+ #average change = new value - old value/old value * 100 ; x = rows
     for x in range(len(revenue)):
         average_change = (revenue[x + 1] - revenue[x])/revenue[x] * 100
         # print(average_change)
