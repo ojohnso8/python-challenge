fddc189e4ac8bf29eead03b6d8eb519560d555ac
ojohnso8 <46421819+ojohnso8@users.noreply.github.com>
2019-03-29 17:15:23 -0400

    Sixth Commit

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -5,10 +5,19 @@ import csv
 #csv path
 election_csv = os.path.join(".", "election_data.csv")
 
+#empty dictionary
+
+poll_data = {}
+poll_percentage = {}
+
 #empty lists
 
 votes = []
 candidateList= []
+unique_candidate_list = []
+winning_count = 0
+winning_candidate = ""
+winner_list = []
 
 #open csv
 with open(election_csv, newline = "") as csvfile:
@@ -16,23 +25,45 @@ with open(election_csv, newline = "") as csvfile:
     csv_header = next(csvreader)
 
     for row in csvreader:
-    
+
         votes.append(row[0])
-        candidateList.append(row[2])
+        # candidateList.append(row[2])
+        candidate_name = row[2]
+        if candidate_name not in unique_candidate_list:
+            unique_candidate_list.append(candidate_name)
+# The total number of votes each candidate won   
+            poll_data[candidate_name] = 0 #reset count
+        poll_data[candidate_name] += 1 #adding up votes; total votes
+    print(unique_candidate_list) #checkwork
+    print(poll_data) #checkwork
 
-  # The total number of votes cast  
+# The total number of votes cast  
     total_votes = len(votes)
-    print("Total Votes: ", total_votes)
-
-# A complete list of candidates who received votes
-    x = candidateList
-    unique_candidate_list = list(set(x))
-    print("Candidates who received votes: ", unique_candidate_list)
+    print("Total Votes: ", total_votes) #checkwork
 
 # The percentage of votes each candidate won
-
-
-
-# The total number of votes each candidate won
+    for (key, value) in poll_data.items():
+        poll_percentage[key] = round(float(value/total_votes * 100), 2)
+    print(poll_percentage)
 
 # The winner of the election based on popular vote.
+    for candidate in poll_data:
+        votes = poll_data[candidate]
+        if votes > winning_count:
+            winning_count = votes
+            winning_candidate = candidate  
+    print(winning_candidate) #check work
+    print(winning_count) #check work
+
+
+#write output
+output_path = os.path.join(".", "election_results.txt")
+
+with open(output_path, 'w') as writefile:
+    writefile.writelines('Election Results' + '\n')
+    writefile.writelines('------------------------' + '\n')
+    writefile.writelines('Total Votes: ' + str(total_votes) + '\n')
+    writefile.writelines('------------------------' + '\n')
+    # writefile.writelines('Total $' + str(total_amount) + '\n')
+    writefile.writelines('------------------------' + '\n')
+    writefile.writelines('Winner: ' + winning_candidate + '\n')
